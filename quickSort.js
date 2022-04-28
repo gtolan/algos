9b4236afb9b2655c07c0906afd1bab05d85308ff
Gerard Tolan <gerardtolan@Gerards-MacBook-Pro.local>
2022-04-28 18:46:42 +0200

    quick sort finished

--- quickSort.js
+++ quickSort.js
@@ -2,19 +2,27 @@
 //bigger values to one side of the pivot smaller to the other
 //recursive breaking list smaller 
 
-const partition = (arr,left,right) => {
 
+const quickSortRecursive = (originalList)=> {
+    let list = [...originalList];
+    //return if list is a single value
+    if(list.length < 2) return list;
+    //create a pivot point where to place smaller 
+    //and bigger values on either side
+    //then spread the smaller, pivot and bigger into
+    //a sorted array
+    const pivot = list[Math.floor((list.length)/2)]; //chedck performance v list[0]
+    let smaller = list.filter(value => value < pivot);
+    let bigger = list.filter(value => value > pivot);
 
+    let sorted = [
+      ...quickSortRecursive(smaller),
+      pivot,
+      ...quickSortRecursive(bigger),
+    ];
+    return sorted;
 }
 
-const quickSortRecursive = (arr,start, end)=> {
 
-
-
-}
-
-const quickSort = (arr) => {
-    quickSortRecursive(arr, start =0, end=arr.length-1)
-}
-
-const arr = [1,4,53,45,2,4,9,14,23]
+const arr = [1,4,53,45,2,4,9,14,23];
+quickSortRecursive(arr)
