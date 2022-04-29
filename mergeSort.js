6e619a7ad8510035d52f352c0647710743a3bcbc
Gerard Tolan <gerardtolan@Gerards-MacBook-Pro.local>
2022-04-29 13:17:44 +0200

    added merge sort

--- mergeSort.js
+++ mergeSort.js
@@ -12,16 +12,15 @@ const merge = (left, right) => {
     return result;
 }
 
-
-let arr = [1,4,11,2,5,2,6,9,10];
-
 const mergeSort = (arr) => {
-    console.log('Result',arr)
+
     if (arr.length < 2) return arr;
    
     let midIndex = Math.floor(arr.length / 2);
-    let arr1 = arr.splice(0,midIndex);
-        console.log('arr1',arr1, 'arrRest',arr)
+    let arr1 = arr.splice(0,midIndex); 
+       
     return merge(mergeSort(arr1),mergeSort(arr))
 }
+
+let arr = [1, 4, 11, 2, 5, 2, 6, 9, 10];
 mergeSort(arr);
